e12572a13baee19e3e7a0e593b18797c276b233f
Oleksandr Konstantinov <iamkonst17@gmail.com>
2018-10-01 20:14:42 +0300

    trying to make redirect work

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 import telebot
-import sys
+import requests
+import urllib
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 IS_OFFLINE = os.environ.get('IS_OFFLINE', 0)
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+REDIRECT_URI = os.environ.get('REDIRECT_URI')
 print("BOT_TOKEN is " + BOT_TOKEN)
 bot = telebot.TeleBot(BOT_TOKEN)
 
@@ -13,14 +16,22 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def Help(message):
     bot.send_message(message.chat.id, "Hi, I'm ready")
 
+@bot.message_handler(commands=['start'])
+def Start(message):
+    payload = {'redirect_uri' : REDIRECT_URI, 'client_id' : CLIENT_ID, 'response_type' : "code"}
+    url = "https://api.instagram.com/oauth/authorize/?" + urllib.parse.urlencode(payload)
+
+    #r = requests.get("https://api.instagram.com/oauth/authorize", payload)
+    bot.send_message(message.chat.id, url)
+
 @app.route('/')
 def hello():
     return "Hello world!"
 
 @app.route('/redirect')
 def redirect():
-    update = request.get_json()
-    print("redirect request: " + str(update))
+    code = request.args.get('code')
+    print("redirect request code: " + code)
     return "Hello redirect!"
 
 @app.route("/{}".format(BOT_TOKEN), methods=["POST"])
